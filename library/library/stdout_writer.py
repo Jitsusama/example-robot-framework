b1344c6afcee49669cdd5293ed9ee2152bcd49f7
Joel Gerber <joel@grrbrr.ca>
2023-03-12 11:44:47 -0400

    Tweak shared library

--- library/library/stdout_writer.py
+++ library/library/stdout_writer.py
@@ -1,13 +1,7 @@
-import sys
-from typing import TextIO
-
-
 class Writer:
-    def __init__(self, stdout: TextIO = None):
-        self.stdout = stdout if stdout else sys.stdout
-
+    # noinspection PyMethodMayBeStatic
     def write_to_stdout(self, message: str) -> str:
         """Writes the given message to STDOUT."""
         msg = f"message: {message}"
-        print(msg, file=self.stdout)
+        print(msg)
         return msg
